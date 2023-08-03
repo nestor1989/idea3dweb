fdcf2a4f9c3ee9f6ebfdcca830c5256b549fdeeb
nestor1989 <nestordelrio1989@gmail.com>
2023-08-03 20:53:44 -0300

    added buttons

---
+++ src/components/ButtonInfo.tsx
@@ -0,0 +1,47 @@
+import { Image, StyleSheet, TouchableOpacity } from "react-native";
+
+
+interface Props{
+    onPress: () => void;
+    icon: any;
+}
+
+const ButtonInfo = ({onPress, icon}: Props)=>{
+    return(
+        <TouchableOpacity 
+        style={styles.button} 
+        onPress={onPress}
+        activeOpacity={0.5}>
+
+          <Image
+            source={icon}
+            style={styles.buttonImageIconStyle}
+            />
+      
+    </TouchableOpacity>
+    
+    );
+}
+
+const styles = StyleSheet.create({
+    button: {
+      width:36,
+      height:36,
+      margin:'10%',
+      color: 'white',
+      alignSelf: 'center',
+    },
+    
+    buttonImageIconStyle: {
+      height: '100%',
+      width: '100%',
+      resizeMode: 'stretch',
+      fill: 'white',
+      zIndex: 1,
+      tintColor:'white',
+    },
+  });
+  
+  export default ButtonInfo;
+
+
